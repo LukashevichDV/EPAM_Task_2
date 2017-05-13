e4974749ddeb821ce167689b0b8ffc8eac0bb1af
Dima <LukashevichDV@mail.ru>
2017-05-13 03:07:58 +0300

    part of solution

--- Task_2/Document.cs
+++ Task_2/Document.cs
@@ -5,50 +5,58 @@ namespace Task_2
 {
     public class Document
     {
+
         private StreamReader StreamReader;
         private List<DocPage> Pages;
         private DocPage Page;
         private int NumberOfLinesOnPage;
         private string FileName;
-        private string BufLine = string.Empty;
+
 
         public Document(string fileName, int numberOfLinesOnPage = 40)
         {
             NumberOfLinesOnPage = numberOfLinesOnPage;
             FileName = fileName;
         }
-        private void ReadFileContent()
+
+
+        public Document(string fileName, List<DocPage> pages)
+        {
+            FileName = fileName;
+            Pages = pages;
+        }
+
+
+        private void ReadData()
         {
             StreamReader = new StreamReader(FileName);
             Pages = new List<DocPage>();
 
             string line;
-            int countLines = 1;
-            int pageNumber = 1;
-            Page = new DocPage(pageNumber, NumberOfLinesOnPage);
+            int currentCountLine = 1;
+            int currentPageNumber = 1;
+            Page = new DocPage(currentPageNumber, NumberOfLinesOnPage);
 
-            while ((line = _streamReader.ReadLine()) != null)
+            while ((line = StreamReader.ReadLine()) != null)
             {
-                //When all lines of page are filled adding filled page to list of pages
-                //Creating new page and start filling it again. Counting of lines is starting again
-                if (countLines > _numberOfLinesPerPage)
+                if (currentCountLine > NumberOfLinesOnPage)
                 {
-                    _pages.Add(_page);
-                    pageNumber++;
-                    _page = new Page(pageNumber, _numberOfLinesPerPage);
-                    countLines = 1;
+                    Pages.Add(Page);
+                    currentPageNumber++;
+                    Page = new DocPage(currentPageNumber, NumberOfLinesOnPage);
+                    currentCountLine = 1;
                 }
-
-                //if page isn't filled continue to filling it and counting lines
-                _page.AddLine(line);
-                countLines++;
+                Page.AddLine(line);
+                currentCountLine++;
             }
-            //Adding last page to page list
-            _pages.Add(_page);
+            Pages.Add(Page);
+            StreamReader.Close();
+        }
 
-            //Closing Stream
-            _streamReader.Close();
+        public Document CreateDocument()
+        {
+            ReadData();
+            return new Document(FileName, Pages);
         }
     }
-}
 }
